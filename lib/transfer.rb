8ee7e5d3619d537724f5b7c7aced689c58e3ec5e
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-02 11:37:42 -0700

    transer initialize method done

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,16 @@
 class Transfer
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+
+  def valid?
+    
+  end
 
 end

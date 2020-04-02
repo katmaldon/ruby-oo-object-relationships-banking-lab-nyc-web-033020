8bd70a70248a6be3c11182f68a855e31a3c30d83
github username <github email address>
2020-04-02 14:13:04 -0400

    BankAccount first methods

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,21 @@
 class BankAccount
 
+  attr_reader :name, :status
+  attr_accessor :balance
+
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{@balance}."
+  end
+
 end

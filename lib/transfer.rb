b60ee6a3e86d1dce2484254d02477c3fd5b9217b
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-04-02 12:22:57 -0700

    strggling...

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,9 +14,31 @@ class Transfer
   end
 
   def execute_transaction
-    @sender.balance -= @amount
-    @receiver.balance += @amount
-    @status = "complete"
+    
+    if @status == "complete"
+      "Transaction rejected"
+    elsif @sender.balance < @amount
+      "Transaction rejected."
+    elsif !@sender.valid?
+      "Transaction rejected."
+    elsif !@receiver.valid?
+      "Transaction rejected."
+    else
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
+    end
+
+
+    
+    #  @sender.balance -= @amount
+    #  @receiver.balance += @amount
+    #  @status = "complete"
+    # end
+      
+
+    
+
   end
 
 end
